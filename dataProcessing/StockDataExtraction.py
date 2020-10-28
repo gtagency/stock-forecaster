02bf0e884901915102c17b3271e9a94f581420f6
Sathyanarayan Sudarshan <03.sathyanarayan@gmail.com>
2020-10-28 13:22:09 -0400

    Modulate data extraction into a function
    This change will help load the data into the model easily.

--- dataProcessing/StockDataExtraction.py
+++ dataProcessing/StockDataExtraction.py
@@ -3,20 +3,26 @@ from pandas_datareader import data as pdr
 import yfinance as yf
 import datetime
 import dateutil
+def load_stock_data(tickername):
+    #To get the data in a pandas dataframe
+    yf.pdr_override()
 
-yf.pdr_override()
+    #get dates for today and ten years back
+    end = datetime.date.today()
+    start = end - dateutil.relativedelta.relativedelta(years=10)
 
-#get dates for today and ten years back
-end = datetime.date.today()
-start = end - dateutil.relativedelta.relativedelta(years=10)
+    #input stock name
+    ticker = tickername
 
-#input stock name
-ticker = input('Enter stock ticker:') 
-
-allinfo = pdr.get_data_yahoo(ticker, start, end)
-allinfo['Daily Volatility'] = 100*(allinfo['High']-allinfo['Low'])/allinfo['Low']
-allinfo['Daily Change'] = 100*(allinfo['Adj Close']-allinfo['Open'])/allinfo['Open']
-
-print(allinfo.head())
-print(allinfo.tail())
-#print(allinfo.columns)
+    #get stock info for past 10 years and calculate daily volatility and change.
+    try: 
+        allinfo = pdr.get_data_yahoo(ticker, start, end)
+    except:
+        print("Enter a proper ticker!")
+    allinfo['Daily Volatility'] = 100*(allinfo['High']-allinfo['Low'])/allinfo['Low']
+    allinfo['Daily Change'] = 100*(allinfo['Adj Close']-allinfo['Open'])/allinfo['Open']
+    #print(type(allinfo))
+    #print(allinfo.head())
+    #print(allinfo.tail())
+    #print(allinfo.columns)
+    return allinfo
