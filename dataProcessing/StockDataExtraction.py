054dc2640b88e9b267bf366a60a5e2e2e0d3e22d
Pujith Kachana <72110347+pkachana3@users.noreply.github.com>
2020-11-04 17:27:34 -0500

    Update StockDataExtraction.py

--- dataProcessing/StockDataExtraction.py
+++ dataProcessing/StockDataExtraction.py
@@ -3,6 +3,8 @@ from pandas_datareader import data as pdr
 import yfinance as yf
 import datetime
 import dateutil
+import os.path
+
 def load_stock_data(tickername):
     #To get the data in a pandas dataframe
     yf.pdr_override()
@@ -13,21 +15,28 @@ def load_stock_data(tickername):
 
     #input stock name
     ticker = tickername
+    
+    #Pull stock info from CSV if CSV exists
+    if os.path.isfile(ticker + '.csv') :
+        allinfo = pd.read_csv (ticker + '.csv')
+        #print(allinfo.head())
+        #print(allinfo.tail())
 
     #get stock info for past 10 years and calculate daily volatility and change.
-    try: 
-        allinfo = pdr.get_data_yahoo(ticker, start, end)
-    except:
-        print("Enter a proper ticker!")
-    allinfo['Daily Volatility'] = 100*(allinfo['High']-allinfo['Low'])/allinfo['Low']
-    allinfo['Daily Change'] = 100*(allinfo['Adj Close']-allinfo['Open'])/allinfo['Open']
-    #print(type(allinfo))
-    #print(allinfo.head())
-    #print(allinfo.tail())
-    #print(allinfo.columns)
+    else:
+        try: 
+            allinfo = pdr.get_data_yahoo(ticker, start, end)
+        except:
+            print("Enter a proper ticker!")
+        allinfo['Daily Volatility'] = 100*(allinfo['High']-allinfo['Low'])/allinfo['Low']
+        allinfo['Daily Change'] = 100*(allinfo['Adj Close']-allinfo['Open'])/allinfo['Open']
+        #print(type(allinfo))
+        #print(allinfo.head())
+        #print(allinfo.tail())
+        #print(allinfo.columns)
 
-    # creates a csv file to hold data
-    allinfo.to_csv('data/' + ticker + '.csv')
+        # creates a csv file to hold data
+        allinfo.to_csv('data/' + ticker + '.csv')
 
 #sample input
 #load_stock_data('AAPL')
