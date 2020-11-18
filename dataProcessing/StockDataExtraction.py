23cd93204352ebec68d93fad6c702fb3842942db
Pujith Kachana <72110347+pkachana3@users.noreply.github.com>
2020-11-18 18:07:48 -0500

    Update StockDataExtraction.py

--- dataProcessing/StockDataExtraction.py
+++ dataProcessing/StockDataExtraction.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import datetime
 import dateutil
 import os.path
+import ta
 
 def load_stock_data(tickername):
     #To get the data in a pandas dataframe
@@ -15,23 +16,38 @@ def load_stock_data(tickername):
 
     #input stock name
     ticker = tickername
+    
+    #Pull stock info from CSV if CSV exists
+    if os.path.isfile(ticker + '.csv') :
+        allinfo = pd.read_csv (ticker + '.csv')
+        print(allinfo.head())
+        print(allinfo.tail())
 
-    #get stock info for past 10 years and calculate daily volatility and change.
-    try:
-        allinfo = pdr.get_data_yahoo(ticker, start, end)
-    except:
-        print("Enter a proper ticker!")
-    allinfo['Daily Volatility'] = 100*(allinfo['High']-allinfo['Low'])/allinfo['Low']
-    allinfo['Daily Change'] = 100*(allinfo['Adj Close']-allinfo['Open'])/allinfo['Open']
-    #print(type(allinfo))
-    #print(allinfo.head())
-    #print(allinfo.tail())
-    #print(allinfo.columns)
+    #get stock info for past 10 years and calculate daily volatility, change, moving average, and RSI.
+    else:
+        try: 
+            allinfo = pdr.get_data_yahoo(ticker, start, end)
+        except:
+            print("Enter a proper ticker!")
+        allinfo['Daily Volatility'] = 100*(allinfo['High']-allinfo['Low'])/allinfo['Low']
+        allinfo['Daily Change'] = 100*(allinfo['Adj Close']-allinfo['Open'])/allinfo['Open']
+        indicator_bb = ta.volatility.BollingerBands(close=allinfo["Adj Close"], n=20, ndev=2)
+        indicator_RSI = ta.momentum.RSIIndicator(close=allinfo["Close"], n=14, fillna =False)
+        allinfo['BB_Moving Avg'] = indicator_bb.bollinger_mavg()
+        allinfo['RSI'] = indicator_RSI.rsi()
+        
+        #Method to remove NaN values
+        #df = ta.utils.dropna(df)
+        
+        #print(type(allinfo))
+        #print(allinfo.head())
+        #print(allinfo.tail())
+        #print(allinfo.columns)
 
-    # creates a csv file to hold data
-    allinfo.to_csv('../data/' + ticker + '.csv')
+        # creates a csv file to hold data
+        allinfo.to_csv(ticker + '.csv')
 
 #sample input
-load_stock_data('AAPL')
+load_stock_data('aapl')
 
 
